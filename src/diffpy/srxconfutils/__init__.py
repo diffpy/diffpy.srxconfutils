44f7f5d9f91b81f3aa95eca409ab31f275c6c7b7
Zhi Ming Xu <zx2525@columbia.edu>
2025-09-20 18:02:12 -0400

    skpkg: migrate src folder

--- src/diffpy/srxconfutils/__init__.py
+++ src/diffpy/srxconfutils/__init__.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# dpx.confutils     by Simon J. L. Billinge group
-#                   (c) 2013 Trustees of the Columbia University
-#                   in the City of New York.  All rights reserved.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
 #
-# File coded by:    Xiaohao Yang
+# File coded by: Billinge Group members.
 #
-# See AUTHORS.txt for a list of people who contributed.
-# See LICENSENOTICE.txt for license information.
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.srxconfutils/graphs/contributors
+#
+# See LICENSE.rst for license information.
 #
 ##############################################################################
+"""Configuration utilities for dpx project.
+
+Part of xPDFsuite
+"""
 
 # package version
-from diffpy.srxconfutils.version import __version__
-
-
-# unit tests
-def test():
-    """Execute all unit tests for the diffpy.pdfgetx package.
-
-    Return a unittest TestResult object.
-    """
-    from dpx.confutils.tests import test
-
-    return test()
-
+from diffpy.srxconfutils.version import __version__  # noqa
 
 # silence the pyflakes syntax checker
 assert __version__ or True
