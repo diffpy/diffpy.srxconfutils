9691ebc607bedda4d6b6f830407d792ce2595aef
Zhi Ming Xu <zx2525@columbia.edu>
2025-09-23 14:56:18 -0400

    chore: fix copyright dates and delete dpx

--- src/diffpy/srxconfutils/version.py
+++ src/diffpy/srxconfutils/version.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2025 The Trustees of Columbia University in the City of New York.
+# (c) 2013-2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Billinge Group members.
+# File coded by: Xiaohao Yang and Billinge Group members.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.srxconfutils/graphs/contributors  # noqa: E501
